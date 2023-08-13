446b41e6522206adb297bacc3c1c5a4220cb7f3d
Damien White <damienkwhite@gmail.com>
2023-08-13 14:56:03 +0100

    added success screen

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ document.getElementById("email").addEventListener("change", () => {
     if (!input.value.includes("@")){
         input.classList.add("formErr")
         document.getElementsByTagName("span")[0].style.display = "inline";
-    } else {
+    } else if (input.value.includes("@")){
         input.classList.remove("formErr")
+        document.getElementsByTagName("span")[0].style.display = "none";
     }
 })
 
